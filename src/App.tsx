47bde47947fe280cd394a2137f19da21c6b35150
Abderelk <abderrahmanelkafif@gmail.com>
2026-02-04 10:17:25 +0100

    fix recovery message

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ function App() {
           <Routes>
             {/* Routes publiques (sans Layout) */}
             <Route path="/" element={<Login />} />
+            <Route path="/login" element={<Login />} />
             <Route path="/register" element={<Register />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
             <Route path="/reset-password" element={<ResetPassword />} />
